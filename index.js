4b5e056bcca64e7f5634cf2eb62f4acbc3b16a02
MikeAMV <92051208+MikeAMV@users.noreply.github.com>
2023-10-07 09:19:11 -0600

    [UPDATE] Adding new dependencies, configurations and tests

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World22');
-})
+});
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
-})
+module.exports = app;
