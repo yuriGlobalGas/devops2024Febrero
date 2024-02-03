75ed2ef20a2069590a644aa279533231245b52ce
yuriGlobalGas <132495021+yuriGlobalGas@users.noreply.github.com>
2024-02-03 12:15:24 -0600

    3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   const podName = os.hostname();
-  res.send(`Hello World from 3`);
+  res.send(`Hello World 3`);
 });
 
 module.exports = app;
